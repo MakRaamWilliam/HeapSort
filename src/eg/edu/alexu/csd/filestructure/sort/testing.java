a7865582bc649404bb4eb364572310685306edd3
MakRaamWilliam <makrm.wilyam99@gmail.com>
2020-03-16 22:09:05 +0200

    lab1

--- src/eg/edu/alexu/csd/filestructure/sort/testing.java
+++ src/eg/edu/alexu/csd/filestructure/sort/testing.java
@@ -1,46 +1,40 @@
 package eg.edu.alexu.csd.filestructure.sort;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class testing {
 
-/*	public static void main(String[] args) {
-	
-		ArrayList<Integer> arr = new ArrayList();
-     arr.add(8); arr.add(6); arr.add(3); arr.add(1); 
 
-        Sort he = new Sort();
+    public static void main(String[] args) throws FileNotFoundException    {
+    	  PrintStream slow = new PrintStream(new FileOutputStream("sloow.txt"));
+    	  PrintStream fast = new PrintStream(new FileOutputStream("faast.txt"));
+        ArrayList<Integer> unordered = new ArrayList(); long t; 
+        ISort tests = new Sort();
         
-        he.sortFast(arr);
-    for(int i=0;i<arr.size();i++) {
-    	System.out.println(arr.get(i));
+       int ip; Random r = new Random();
+       for(int i=1000;i<=100000;i+=1000) {
+    	   unordered.clear();
+    	   for(int j=0;j<i;j++) {
+    	   unordered.add(r.nextInt(50000));  		
+    	   }
+    	  long start= System.currentTimeMillis();
+    	  tests.sortSlow(unordered);  
+     	  t= System.currentTimeMillis()-start;
+    	  System.setOut(slow);
+   	      System.out.println(i + "\t" + t);
+    	  start = System.currentTimeMillis();
+    	  tests.sortFast(unordered);
+    	  t=System.currentTimeMillis()-start;
+    	  System.setOut(fast);
+    	  System.out.println(i + "\t" + t);
+       } 
+        
     }
-	}
-/*	public void quicksort(Object arr[],int l,int r) {
-		if(l >= r) return;
-		
-		int p=getpivot(l, r, arr);
-		quicksort(arr, l, p-1);
-		quicksort(arr,p+1 , r);
-	}
-	private int  getpivot(int l,int r,Object []arr) {
-		T pivot =(T) arr[l]; int p=l; 
-		int i=l, j=r;
-		while(i<j) {
-			if(pivot.compareTo((T) arr[i]) == 0) {
-				if(((T) arr[j]).compareTo(pivot) >=0) j--;
-				else {
-					swap(i,j,arr); p=j; i++;
-				}
-			}
-		   else {
-			   if(((T) arr[i]).compareTo(pivot) <=0) i++;
-			   else {
-				   swap(i,j,arr); p=i; j--;
-			   }
-		   }
-		}	 
-    return p;
-	}    */
+	
+
 
 }
